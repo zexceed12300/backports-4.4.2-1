94785c50d341fa03d2a78d4b025212e05be45e6b
zexceed12300 <zexceed12300@gmail.com>
2020-07-13 18:58:43 +0000

    remove: current_user_ns

--- backport-include/linux/cred.h
+++ backport-include/linux/cred.h
@@ -3,8 +3,4 @@
 #include_next <linux/cred.h>
 #include <linux/version.h>
 
-#ifndef current_user_ns
-#define current_user_ns()	(current->nsproxy->user_ns)
-#endif
-
 #endif /* __BACKPORT_LINUX_CRED_H */
